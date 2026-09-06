2266c7f1a618095852426522179173fb3a9a9503
Damayanthi <your-email@example.com>
2026-09-06 16:22:40 +0530

    vessal model

--- inspections/models.py
+++ inspections/models.py
@@ -6,6 +6,9 @@ class Vessel(models.Model):
     vessel_name = models.CharField(max_length=150)
 
     active = models.BooleanField(default=True)
+    scoc_active = models.BooleanField(
+        default=False
+    )
 
     def __str__(self):
         return self.vessel_name
